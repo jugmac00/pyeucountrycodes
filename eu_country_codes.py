37e509c172d77f1d48b636a40e29b75d50a06341
Juergen Gmach <juergen.gmach@apis.de>
2019-01-15 16:51:01 +0100

    Add module with country codes.
    The module contains
    - the dictionary
    - the version
    - and a short docstring.

---
+++ eu_country_codes.py
@@ -0,0 +1,34 @@
+"""All ISO 3166 country codes for eu countries."""
+
+__version__ = "0.1"
+
+COUNTRY_CODES = [
+    "AT",
+    "BE",
+    "BG",
+    "CY",
+    "CZ",
+    "DE",
+    "DK",
+    "EE",
+    "ES",
+    "FI",
+    "FR",
+    "GB",
+    "GR",
+    "HR",
+    "HU",
+    "IE",
+    "IT",
+    "LT",
+    "LU",
+    "LV",
+    "MT",
+    "NL",
+    "PL",
+    "PT",
+    "RO",
+    "SE",
+    "SI",
+    "SK",
+]
